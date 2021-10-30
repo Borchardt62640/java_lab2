4a5695720d06642b3bc6b6f11db2889447882f09
Borchardt62640 <piotr3borchardt@gmial.com>
2021-10-30 09:47:07 +0200

    cw2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,11 +10,23 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+//        for(int i = 100;  i >= 0; i--){
+//
+//            if( i % 40 == 0)
+//            System.out.print("\nindex: " + i + " " + (i % 40));
+//        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
-
+//        Scanner scan = new Scanner(System.in);
+//        String[] tab1 = new String[5];
+//        for(int i = 0;  i < tab1.length; i++){
+//            tab1[i] = scan.next();
+//        }
+//        for(int i = 0;  i < tab1.length; i++){
+//            System.out.print(tab1[i]);
+//        }
     }
 }
