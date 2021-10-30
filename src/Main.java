dfe2919d4a314b2e32c168cff74f6b52751b713c
Borchardt62640 <piotr3borchardt@gmial.com>
2021-10-30 10:18:09 +0200

    cw3

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,60 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
-
+//        Scanner scan = new Scanner(System.in);
+//        int a = Integer.parseInt(scan.next());
+//        System.out.print(a);
+//        switch (a % 2){
+//            case 0:
+//                System.out.print("\nparzysta");
+//                break;
+//            default:
+//                System.out.print("\nnieparzysta");
+//                break;
+//        }
+//        switch (a % 7){
+//            case 0:
+//                System.out.print("\npodzielna przez 7");
+//                break;
+//            default:
+//                System.out.print("\nniepodzielna przez 7");
+//                break;
+//        }
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
 
+//        String a = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+//        char[] ch = a.toCharArray();
+//        int i = 0;
+//        int y = 0;
+//        for(int j = 0; j < a.length(); j++){
+//            System.out.print("\n" + ch[j]);
+//            switch (ch[j]){
+//                case 'o':
+//                    i++;
+//                break;
+//                case 'p':
+//                    i++;
+//                break;
+//                case 'x':
+//                    i++;
+//                break;
+//                case 'z':
+//                    i++;
+//                break;
+//                case 'q':
+//                    i++;
+//                break;
+//                default:
+//                    y++;
+//                break;
+//            }
+//        }
+//        System.out.print("\nnalezy = " + i);
+//        System.out.print("\nnienalezy = " + y);
 
     }
 }
