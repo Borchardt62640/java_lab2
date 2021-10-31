a0904959672cdab11cbc52185ceb0146a8696ce2
Borchardt62640 <piotr3borchardt@gmial.com>
2021-10-31 09:47:20 +0100

    cw5

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,51 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
 
+    // Zad 1
+    public static void zad_1(int[] tab){
+        for(int i = 19; i>=0; i--){
+            tab[i] = i + 20;
+            System.out.println("tab[" + i + "] = " + tab[i]);
+        }
+    }
+
+    // Zad 2
+    public static void zad_2(String[] tab){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 6; i++){
+            System.out.println("podaj wartosc");
+            tab[i] = sc.next();
+        }
+        for(int i = 5; i >= 0; i--){
+            System.out.println("tab[" + i + "] = " + tab[i]);
+        }
+    }
+
+    // Zad 3
+    public static void zad_3(String imie){
+        System.out.println("imie "+ imie);
+    }
+    public static void zad_3(String imie, String nazwisko){
+        System.out.println("imie "+ imie);
+        System.out.println("nazwisko "+ nazwisko);
+    }
+    public static void zad_3(String imie, String nazwisko, int wiek){
+        System.out.println("imie "+ imie);
+        System.out.println("nazwisko "+ nazwisko);
+        System.out.println("wiek "+ wiek);
+    }
+
+    // Zad 4
+    public static int zad_4(int a, int b){
+        return a + b;
+    }
+    public static int zad_4(int a, int b, int c){
+        return a + b + c;
+    }
     public static void main(String[] args) {
         /* ćwiczenie 5 Metody */
 
@@ -13,12 +56,18 @@ public class Main {
         d) wstawić wywołanie metody do metody main
         */
 
+//        int[] tab_x = new int[20];
+//        zad_1(tab_x);
+
         /* zad.2
         a) stworzyc tablicę typu String[6],
         b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury
             (użyć do tego pętli i klasy Scanner),
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
+
+//        String[] tab_s = new String[6];
+//        zad_2(tab_s);
 
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
@@ -29,6 +78,13 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
+//        String im = "Piotr";
+//        String na = "Borchardt";
+//        int w = 25;
+//        zad_3(im);
+//        zad_3(im, na);
+//        zad_3(im, na, w);
+
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
             coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
@@ -37,7 +93,12 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
-
+//        int a = 3;
+//        int b = 8;
+//        int c = 51;
+//        int d = zad_4(a, b);
+//        int e = zad_4(a, b, c);
+//        System.out.println("wynik : "+ (zad_4(a, b) + zad_4(a, b, c)));
     }
 
 }
